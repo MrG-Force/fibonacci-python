141c748afd1f173b388eb93231a7e200a5ba8af7
MrGForce <willortizloza@hotmail.com>
2022-05-01 21:04:15 +1000

    add test_is_fibonacci

--- test_fibonacci.py
+++ test_fibonacci.py
@@ -5,7 +5,12 @@ import fibonacci as fib
 class TestFibonacci(unittest.TestCase):
 
     def test_get_nth_fibonacci(self):
-        self.assertEqual(fib.get_nth_fibonacci(10), 55, "Expected 55")
+        self.assertEqual(fib.get_nth_fibonacci(10), 34, "Expected 34")
+        self.assertEqual(fib.get_nth_fibonacci(1), 0, "Expected 0")
+
+    def test_is_fibonacci(self):
+        self.assertTrue(fib.is_fibonacci(377))
+        self.assertFalse(fib.is_fibonacci(378))
 
 
 if __name__ == '__main__':
